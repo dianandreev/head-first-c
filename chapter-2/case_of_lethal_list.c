3407b5a5a397c5cbe5a621185325be9e9d2024a4
Dian Andreev <dian_andreev@yahoo.com>
2023-11-15 14:03:17 +0200

    Add case of lethal list example;

---
+++ chapter-2/case_of_lethal_list.c
@@ -0,0 +1,14 @@
+/*
+* Learn more about pointer arithmethics;
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	int doses[] = {1, 3, 2, 1000};
+	printf("Issue dose %i \n", 3[doses]);
+
+	//doses[3] == *(doses + 3) == *(3 + doses) = 3[doses]
+	return 0;
+}
