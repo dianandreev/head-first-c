eeabad8b68a8424e8836c92a5f81572e80f4c329
Dian Andreev <dian_andreev@yahoo.com>
2023-11-15 14:11:58 +0200

    Add swtich example from chapter 1

---
+++ chapter-1/cards_2_swtich.c
@@ -0,0 +1,53 @@
+/*
+* Program to evaluate face values.
+* Released under the Vegas Public License.
+* (c)2014 The College Blackjack Team.
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char card_name[3];
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	int val = 0;
+//	if (card_name[0] == 'K') {
+//		val = 10;
+//	} else if (card_name[0] == 'Q'){
+//		val = 10;
+//	} else if (card_name[0] == 'J') {
+//		val = 10;
+//	} else if(card_name[0] == 'A') {
+//		val = 11;
+//	} else {
+//		val = atoi(card_name);
+//	}
+	switch(card_name[0]) {
+		case 'K':
+			val = 10;
+			break;
+		case 'Q':
+			val = 10;
+			break;
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		default:
+			val = atoi(card_name);
+
+	}
+	printf("The card value is: %i\n", val);
+
+	if (val >= 3 && val <= 6) {
+		puts("Count has gone up");
+	} else if (val == 10) {
+		puts("Count has gone down");
+	}
+
+	return 0;
+}
